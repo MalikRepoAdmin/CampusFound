b6630aee09309b30bcf9af966df60befce3e2aef
MalikRepoAdmin <mohamadmalikfajarbaihaqi@gmail.com>
2026-05-17 10:22:39 +0700

    Temporary Tidy up for auth Routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\LaporanController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -24,9 +25,31 @@ Route::get('/barang/{id}', function ($id) {
 Route::get('/profile', function () {
     return view('items.profile');
 })->name('profile');
-Route::get('/items/{id}/editbarang', function ($id) { return view('items.editBarang'); })->name('items.editBarang');
-Route::delete('/items/{id}', function ($id) {  return redirect()->route('profile'); });
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
 
+/**
+ * Edit Barang Routes
+ */ 
+// This middleware will break demo
+Route::middleware('auth')->group(function () {
+
+    Route::get('/items/{laporan}/editbarang', [LaporanController::class, 'edit'])->name('items.editBarang');
+    Route::put('/items/{laporan}', [LaporanController::class, 'update'])->name('items.update');
+});
+
+
+/**
+ * Auth Routes
+ */ 
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::get('/register', [AuthController::class, 'register']);
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+
+/**
+ * Resources Routes
+ */
+Route::resource('laporan', \App\Http\Controllers\LaporanController::class);
