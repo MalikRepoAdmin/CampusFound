36029712a62d8aa351786d12ea045f10e3c4bf57
MalikRepoAdmin <mohamadmalikfajarbaihaqi@gmail.com>
2026-05-29 19:49:12 +0700

    fix nullable properties and foreignkey in 'klaim' schema

--- database/migrations/2026_05_25_083830_create_klaim_table.php
+++ database/migrations/2026_05_25_083830_create_klaim_table.php
@@ -12,14 +12,17 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('klaim', function (Blueprint $table) {
-            $table->id();
+            $table->id('id_klaim');
             $table->string('ciri'); 
-            $table->string('foto_bukti'); 
-            
-            $table->dateTime('created_at'); 
+            $table->string('foto_bukti')->nullable(); 
             
             $table->unsignedBigInteger('fk_id_user');
             $table->unsignedBigInteger('fk_id_laporan');
+
+            $table->foreign('fk_id_user')->references('id_user')->on('users')->onDelete('cascade');
+            $table->foreign('fk_id_laporan')->references('id_laporan')->on('laporan')->onDelete('cascade');
+
+            $table->timestamps();
 
             });
     }
